54f88edf060024d86701e2a759418d71f36ed844
Zhou Xinzi <imdreamrunner@gmail.com>
2014-11-08 10:28:34 +0800

    modify run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ rm -R ./target/
 echo "Start packaging..."
 mvn package
 echo "Done packaging, start running..."
-cd target && java -jar web-monitor-1.0-SNAPSHOT-jar-with-dependencies.jar
+cd target && java -jar web-monitor-*-jar-with-dependencies.jar
