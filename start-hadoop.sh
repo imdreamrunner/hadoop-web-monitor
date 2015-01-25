2094b5f409dbf75f4c8daf3b2495be8bd8de68d9
Zhou Xinzi <imdreamrunner@gmail.com>
2015-01-25 22:44:22 +0800

    add start hadoop script

---
+++ start-hadoop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$HADOOP_PREFIX" ]; then
+    export HADOOP_PREFIX=/usr/local/hadoop
+fi
+
+# Prepare HDFS
+${HADOOP_PREFIX}/bin/hdfs namenode -format
+${HADOOP_PREFIX}/sbin/start-dfs.sh
+
+# Prepare Yarn
+${HADOOP_PREFIX}/sbin/start-yarn.sh
+
+# Copy File
+${HADOOP_PREFIX}/bin/hdfs -mkdir /input
+if [ -d ../test_images ]; then
+    ${HADOOP_PREFIX}/bin/hdfs -put ../test_images/* /input
+fi
